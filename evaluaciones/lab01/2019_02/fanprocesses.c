d9b8d710153fe66f9d7ddd54fccea9499d472c30
Alumno de Informática <alulab@imglabmp.lab.inf.pucp.edu.pe>
2019-09-02 20:01:45 -0500

    fanprocesses ready to start

--- evaluaciones/lab01/2019_02/fanprocesses.c
+++ evaluaciones/lab01/2019_02/fanprocesses.c
@@ -13,24 +13,8 @@
 */
 
 int main(void) {
-    int i, status;
-    pid_t child; // pid_t es un tipo definido en types.h
-    pid_t first_ppid = getpid();
 
-    for (i=1; i<4; ++i) if (child=fork()) break;
-
-    fprintf(stderr, "Esta es la vuelva Nro %d\n", i);
-    fprintf(stderr, "Recibí de fork el valor de %d\n", child);
-    fprintf(stderr, "Soy el proceso %d con padre %d\n", getpid(), getppid());
-
-    wait(&status);
-
-    // si es el último hijo
-    if (child == 0) {
-        char command[1000];
-        sprintf(command, "pstree -p %ld > chainprocesses.txt", (long)first_ppid);
-        system(command);
-    }
+    
 
     return 0;    
 }
