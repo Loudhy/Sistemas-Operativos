a75be46c24ec1f53c7a34c80b37bac6734a09142
Alumno de Informática <alulab@imglabmp.lab.inf.pucp.edu.pe>
2019-09-02 19:34:35 -0500

    first task completed (a,b,c and d)

--- evaluaciones/lab01/2019_02/mypid.c
+++ evaluaciones/lab01/2019_02/mypid.c
@@ -3,7 +3,7 @@
 #include <sys/wait.h>
 #include <unistd.h>
 #include <stdlib.h>
-
+#include <string.h>
 
 int main(void) {
     pid_t child;
@@ -15,8 +15,22 @@ int main(void) {
     }
     else {
         fprintf(stderr, "Fork() devolvió %d. Por tanto, soy el hijo con ID = %d\n", child, getpid());
-        system("pstree > mypid_procesos.txt");
+        char command[1000];
+
+        // system("pstree > mypid_procesos.txt");                                           // (a)
+        // sprintf(command, "pstree %ld > mypid_procesos_v3.txt", (long)getppid());         // (c) => se imprime en directorio actual
+        sprintf(command, "cd ~; pstree %ld > mypid_procesos_v3.txt", (long)getppid());      // (d) => se imprime en directorio home
+        system(command);
     }
 
     return 0;
 }
+
+/*
+* Respuestas:
+* b)    sh es el intérprete del comando que se ha ejecutado "pstree"
+*   
+* c)    ia está, usa sprintf <3
+
+* d)    agregar "cd ~;" donde ~ representa el home
+*/
