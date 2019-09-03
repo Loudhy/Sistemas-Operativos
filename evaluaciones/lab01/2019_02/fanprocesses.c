02f372c3d5f8a841ff009c9007671b595ad55ce6
Alumno de Informática <alulab@imglabmp.lab.inf.pucp.edu.pe>
2019-09-02 19:54:20 -0500

    part 2 semi completed

---
+++ evaluaciones/lab01/2019_02/fanprocesses.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+/*
+* Este programa crea un abanico de procesos. Es decir, el padre
+* crea tres hijo.
+* Ejm 2.6 del libro UNIX Programación Práctica  - Kay Robbins
+*                                               - Steve Robbins
+* Modificado por Alejandro Bello Ruiz - Informática PUCP
+*
+*/
+
+int main(void) {
+    int i, status;
+    pid_t child; // pid_t es un tipo definido en types.h
+    pid_t first_ppid = getpid();
+
+    for (i=1; i<4; ++i) if (child=fork()) break;
+
+    fprintf(stderr, "Esta es la vuelva Nro %d\n", i);
+    fprintf(stderr, "Recibí de fork el valor de %d\n", child);
+    fprintf(stderr, "Soy el proceso %d con padre %d\n", getpid(), getppid());
+
+    wait(&status);
+
+    // si es el último hijo
+    if (child == 0) {
+        char command[1000];
+        sprintf(command, "pstree -p %ld > chainprocesses.txt", (long)first_ppid);
+        system(command);
+    }
+
+    return 0;    
+}
