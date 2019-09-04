e12dca1a679054f85ec921b12d5960848bf48779
Dan <jhairguz.t@gmail.com>
2019-09-04 15:26:57 -0500

    some progress in printing tree

--- evaluaciones/lab01/2019_02/4_binarytree.c
+++ evaluaciones/lab01/2019_02/4_binarytree.c
@@ -12,35 +12,39 @@ int main(int argc, char const *argv[]) {
     }
 
     int deep, i, procn;
-    pid_t parent_pid, left_child, right_child;
-
+    
     deep = atoi(argv[1]);
     procn = 1;
 
-    for (i=1; i<=deep; ++i) {
-        parent_pid = getpid();
-        fprintf(stderr, "Soy el proceso %d [PID=%5.5ld]\n", procn, (long)getpid());
-        if (i == deep) break;
+    pid_t pid_left, pid_right, pid_parent;
+    fprintf(stderr, "Soy el proceso %d [PID=%ld -- PPID=%ld]\n", procn, (long)getpid(), (long)getppid());
         
-        // left side
-        if ((left_child = fork()) == 0) {
+    for (i=1; i<=deep; ++i) {
+        pid_parent = getpid();
+        if (i == deep) break;
+
+        // left code
+        if ((pid_left=fork()) == 0) {
             procn = procn * 2;
-            waitpid(right_child, NULL, 0);
+            fprintf(stderr, "Soy el proceso %d [PID=%ld -- PPID=%ld]\n", procn, (long)getpid(), (long)getppid());
+            waitpid(pid_left, NULL, 0);
         }
 
-        // right side, just parent can create the right child
-        if ((parent_pid==getpid()) && ((right_child=fork())==0)) {
+        // right code
+        if ((pid_parent==getpid()) && (pid_right=fork()) == 0) {
             procn = procn * 2 + 1;
-            waitpid(left_child, NULL, 0);
+            fprintf(stderr, "Soy el proceso %d [PID=%ld -- PPID=%ld]\n", procn, (long)getpid(), (long)getppid());
+            continue;
+            // waitpid(pid_left, NULL, 0);
         }
 
         // parent code
-        if (parent_pid == getpid()) {
-            waitpid(left_child, NULL, 0);
-            waitpid(right_child, NULL, 0);
+        if (pid_parent == getpid()) {
+            waitpid(pid_right, NULL, 0);
             break;
         }
-    }    
+    }
 
     return 0;
 }
+
