897b3f9ac93fdeb193f903643829a9bb562edd13
Dan <jhairguz.t@gmail.com>
2019-09-04 10:35:53 -0500

    task 2, part b completed

--- evaluaciones/lab01/2019_02/fanprocesses.c
+++ evaluaciones/lab01/2019_02/fanprocesses.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
@@ -14,7 +15,30 @@
 
 int main(void) {
 
-    
+    int i, status;
+    pid_t child, pid_padre;
 
-    return 0;    
+    pid_padre = getpid();
+
+    for (i=1; i<4; ++i) {
+        // child code
+        if ((child=fork()) <= 0) {
+            sleep(5);
+            break;
+        }
+        // parent code
+        else fprintf(stderr, "Este es el ciclo Nro %d y se está creando el proceso %d\n", i, child);
+    }
+        
+    // take picture & wait for children
+    if (pid_padre == getpid()) {
+        char command[100];
+        sprintf(command, "pstree -p %ld > fanprocesses.txt", (long)pid_padre);
+        system(command);
+
+        for (i=1; i<4; ++i) wait(&status);
+    }
+
+    fprintf(stderr, "Este es el proceso %d con padre %d\n", getpid(), getppid());
+    return 0;
 }
