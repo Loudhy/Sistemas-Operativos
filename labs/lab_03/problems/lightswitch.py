2c7ea8d9e3bb9f1c51c7fba35249ea82a8368506
Alumno de Informática <alulab@imglabmp.lab.inf.pucp.edu.pe>
2019-10-02 11:25:15 -0500

    pattern LightSwitch implemented for reader-writer problem

---
+++ labs/lab_03/problems/lightswitch.py
@@ -0,0 +1,20 @@
+from threading import Semaphore
+
+class Lightswitch:
+
+    def __init__(self):
+        self.counter = 0
+        self.mutex   = Semaphore(1)
+    
+    
+    def lock(self, semaphore):
+        with self.mutex:
+            self.counter += 1
+            if self.counter == 1:
+                semaphore.acquire()
+        
+    def unlock(self, semaphore):
+        with self.mutex:
+            self.counter -= 1
+            if self.counter == 0:
+                semaphore.release()
